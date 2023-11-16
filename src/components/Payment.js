5bb416b3cd88947a44c28d10c84491bf38c288c7
Shree Vatsa <vastas2002@gmail.com>
2023-11-16 11:49:09 +0530

    Rechanged a few things

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -12,27 +12,27 @@ function Payment() {
   const { walletAddress } = useParams();
   const { apiKey } = useParams();
 
-  const getKey = async() => {
-    try{
-       const res  = await axios.get(`http://localhost:5000/api/login?apiKey=${apiKey}`);
-       setUser(res?.data.user)
+  const getKey = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:5000/api/login?apiKey=${apiKey}`
+      );
+      setUser(res?.data.user);
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-       console.log(err);
-    }
-  }
+  };
 
-useEffect(() => {
-  getKey();
-}, [apiKey])
-
+  useEffect(() => {
+    getKey();
+  }, [apiKey]);
 
   async function connectWallet() {
     const icpCanisterId = "ryjl3-tyaaa-aaaaa-aaaba-cai";
     const whitelist = [icpCanisterId];
     const publicKey = await window.ic.plug.requestConnect({ whitelist });
     console.log("Connected to ", publicKey);
-    
+
     console.log(await window.ic.plug.isConnected());
     console.log(window.ic.plug.sessionManager.sessionData);
     console.log(window.ic.plug.sessionManager.sessionData.principalId);
@@ -47,44 +47,46 @@ useEffect(() => {
 
   async function pay() {
     const transferArgs = {
-      to: "WALLET ADDRESS OF THE OWNER",
-      amount: subscriptionFee * (10 ^ 8),
+      to: user?.walletAddress,
+      amount: user?.amount * (10 ^ 8),
       memo: new Uint16Array(8),
     };
 
     await window.ic.plug.requestTransfer(transferArgs);
-    const res  =  axios.get(`${user?.callBackUrl}?apiKey=${apiKey}&walletAddress=${walletAddress}&subscriptionfess=true`) // call the call backurl with the payment details
+    const res = axios.get(
+      `${user?.callBackUrl}?apiKey=${apiKey}&walletAddress=${walletAddress}&subscription=true`
+    ); // call the call backurl with the payment details
   }
 
   return (
-    <div className='dashboard'>
-    <div className="dashboard-container">
-      <h2>Payment</h2>
-      <div className='dashboard-stats'>
-        <div className="stat">
-          <div className="stat-value">{user?.amount}</div>
-          <div className="stat-label">Subscription Fee</div>
+    <div className="dashboard">
+      <div className="dashboard-container">
+        <h2>Payment</h2>
+        <div className="dashboard-stats">
+          <div className="stat">
+            <div className="stat-value">{user?.amount}</div>
+            <div className="stat-label">Subscription Fee</div>
+          </div>
+          <div className="stat">
+            <div className="stat-value">{user?.details}</div>
+            <div className="stat-label">Details</div>
+          </div>
+          <div className="stat">
+            <div className="stat-value">{user?.walletAddress}</div>
+            <div className="stat-label">Owner</div>
+          </div>
         </div>
-        <div className="stat">
-          <div className="stat-value">{user?.details}</div>
-          <div className="stat-label">Details</div>
-        </div>
-        <div className="stat">
-          <div className="stat-value">{user?.walletAddress}</div>
-          <div className="stat-label">Owner</div>
-        </div>
-      </div>
-      <div>
-        <div className="stat">
-        {wallet==user?.walletAddress? (
-        <button onClick={pay}>Pay</button>
-      ) : (
-        <button onClick={connectWallet}>Connect Wallet</button>
-      )}
+        <div>
+          <div className="stat">
+            {wallet == user?.walletAddress ? (
+              <button onClick={pay}>Pay</button>
+            ) : (
+              <button onClick={connectWallet}>Connect Wallet</button>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
